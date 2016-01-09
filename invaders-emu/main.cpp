bf4989b6fc6c12048f03cd88260f11bee7aea62a
miyole <mlee1746@gmail.com>
2016-01-09 15:05:09 -0500

    Initial Commit

---
+++ invaders-emu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  invaders-emu
+//
+//  Created by Michael Y. Lee on 1/9/16.
+//  Copyright © 2016 Michael Y. Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
